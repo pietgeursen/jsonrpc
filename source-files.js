de00856f7a8ff19cf2dca0ee391c82303ac33226
Travis CI User <travis@example.org>
2019-06-05 13:40:33 +0000

    Update documentation

---
+++ source-files.js
@@ -0,0 +1,15 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["jsonrpc_client_transports"] = {"name":"","dirs":[{"name":"transports","dirs":[],"files":["duplex.rs","http.rs","local.rs","mod.rs","ws.rs"]}],"files":["lib.rs"]};
+sourcesIndex["jsonrpc_core"] = {"name":"","dirs":[{"name":"types","dirs":[],"files":["error.rs","id.rs","mod.rs","params.rs","request.rs","response.rs","version.rs"]}],"files":["calls.rs","delegates.rs","io.rs","lib.rs","middleware.rs"]};
+sourcesIndex["jsonrpc_core_client"] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex["jsonrpc_derive"] = {"name":"","dirs":[],"files":["lib.rs","options.rs","rpc_attr.rs","rpc_trait.rs","to_client.rs","to_delegate.rs"]};
+sourcesIndex["jsonrpc_http_server"] = {"name":"","dirs":[],"files":["handler.rs","lib.rs","response.rs","utils.rs"]};
+sourcesIndex["jsonrpc_ipc_server"] = {"name":"","dirs":[],"files":["lib.rs","meta.rs","select_with_weak.rs","server.rs"]};
+sourcesIndex["jsonrpc_pubsub"] = {"name":"","dirs":[],"files":["delegates.rs","handler.rs","lib.rs","oneshot.rs","subscription.rs","typed.rs","types.rs"]};
+sourcesIndex["jsonrpc_pubsub_examples"] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex["jsonrpc_server_utils"] = {"name":"","dirs":[],"files":["cors.rs","hosts.rs","lib.rs","matcher.rs","reactor.rs","session.rs","stream_codec.rs","suspendable_stream.rs"]};
+sourcesIndex["jsonrpc_stdio_server"] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex["jsonrpc_tcp_server"] = {"name":"","dirs":[],"files":["dispatch.rs","lib.rs","meta.rs","server.rs","service.rs"]};
+sourcesIndex["jsonrpc_test"] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex["jsonrpc_ws_server"] = {"name":"","dirs":[],"files":["error.rs","lib.rs","metadata.rs","server.rs","server_builder.rs","session.rs"]};
+createSourceSidebar();
